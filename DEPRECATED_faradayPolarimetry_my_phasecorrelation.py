0e946755513ce475075eef9915f78cba92d290da
unknown <noahdilworth517@gmail.com>
2023-12-06 17:37:57 -0800

    transitioned to use of imreg_dft and calibration files, deprecating old phase correlation method

--- DEPRECATED_faradayPolarimetry_my_phasecorrelation.py
+++ DEPRECATED_faradayPolarimetry_my_phasecorrelation.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+NOTE: This program uses the phase correlation method in phaseCorrelation.py,
+which accounts for translation only, ignoring angle and scale.
+Use faradayPolarimetry instead for angle and scale, using imreg_dft module
 """
 
 #%%
@@ -14,15 +14,13 @@ import tkinter as Tkinter, tkinter.filedialog as tkFileDialog
 from PIL import Image
 import matplotlib.pyplot as plt
 import phaseCorrelation
-import imreg_dft as ird
-import os
-import json
 
 #%%
 """
 OPTIONS
 """
-# TODO: writeup user instructions for this program
+phasecorr_margin = 0
+phasecorr_plotting = False
 
 #%%
 """ 
@@ -33,42 +31,45 @@ V = 31.4    #[rad/(T*m)]Verdet constant
 L = 0.011   #[m]Faraday glass length
 #%%
 """
-UI
+FILE IMPORTS
 """
-# create tkinter root
-root = Tkinter.Tk()
-root.withdraw()
-root.wm_attributes("-topmost", 1)
-
 # this function opens a file select dialog through Tkinter and returns 
 # the path to the selected file
 def getfile(message):
+    root = Tkinter.Tk()
+    root.after(100, root.focus_force)
+    root.after(200,root.withdraw)    
     filepath = tkFileDialog.askopenfilename(parent=root,title=message)    
     return filepath 
 
-# this function opens a question dialog with 'yes' and 'no' buttons,
-# returning the string 'yes' or 'no' depending on the choice
-def ask(question):
-    answer = Tkinter.messagebox.askquestion(message=question, parent=root)
-    return answer
-
-#%%
-"""
-FILE IMPORT
-"""
 # select bg and shot images, saving filenames and loading images into arrays
 # NOTE: For now, can't figure out how to get PIL to import raw images as 16-bit
 # instead of 8-bit. Just use imagej module to convert to 16-bit tiff first
-names = ["bg1", "shot1", "bg2", "shot2"]
+names = ["bg1", "shot1", "bg2", "shot2", "align1", "align2"]
+# TODO: figure out imagej library or find another way to create a 
+# subselection for alignment
+# NOTE: align images are expected to not be mirrored, as that is required
+# when manually cropping matching subelections in imagej stacks
+# BE CAREFUL that the align images are flipped to match im1
+# else the given translation and crop will flip sign (bc im2 is flipped)
+# TODO: writeup user instructions for this program
 files = {name:getfile("Choose "+name) for name in names} 
 images = {name:np.array(Image.open(files[name])) for name in names}
 #get path to folder containing first file in files
 folder = '/'.join(files['bg1'].split('/')[:-1]) + '/' 
 dateshot = folder.split('/')[-3]    # date and shot# string, eg 110223s4
+
+# show images
+# fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3,2)
+# axes = {"bg1":ax1,"shot1":ax2,"bg2":ax3,"shot2":ax4,
+        # "align1":ax5, "align2":ax6}
+# for name in names:
+#     axes[name].imshow(images[name])
+#     axes[name].set_title(name)
     
 #%%
 """
-NORMALIZATION
+NORMALIZATION AND ALIGNMENT
 """
 # unflip images which was mirrored by the beamsplitter
 images["bg2"] = np.flip(images["bg2"], 1)
@@ -81,68 +82,23 @@ images["shot2"] = np.flip(images["shot2"], 1)
 images["N1"] = images["shot1"]/images["bg1"]
 images["N2"] = images["shot2"]/images["bg2"]
 
-#%%
-"""
-ALIGNMENT
-"""
-# Use calibration file to determine displacement, angle, and scale 
-# which needs to be applied to N2 to align it with N1
-
-# look for calibration file in folder first
-if os.path.isfile(folder+"calibration.json"):
-    with open(folder+"calibration.json") as f:
-        calibration = json.load(f)
-    print("Using calibration file found in folder, from path "
-          +calibration["path"])
-    
-# if calibration in FaradayPolarimetry/calibration exists, 
-# ask if it is ok to use, and if not, ask to choose a calibration file
-elif os.path.isfile("calibration/calibration.json"):
-    with open("calibration/calibration.json") as f:
-        curcal = json.load(f)
-    answer = ask("Is the current calibration, from "+curcal["path"]+
-             ", ok to use?")
-    if answer == "yes":
-        calibration=curcal
-        using_curcal = True
-    else:
-        choosefile = True
-        
-# if no calibration file in image folder or FaradayPolarimetry/calibration
-else:
-    choosefile = True
-
-# choose a calibration file
-if choosefile:
-    file = getfile("Choose a calibration file")
-    with open(file) as f:
-        calibration = json.load(f)
-
-# allow the user to save calibration to folder if it wasn't already
-if choosefile or using_curcal:
-    message = """would you like to save the chosen calibration to the folder 
-    to avoid choosing next time?"""
-    answer = ask(message)
-    if answer == "yes":
-        with open(folder+"calibration.json", 'w') as f:
-            json.dump(calibration, f, indent=1)
-
-# use calibration to scale and rotate N2 using imreg_dft
-# 'scangle' stands for 'scale and angle'
-images["N2_scangle"] = ird.transform_img(images["N2"], 
-    scale=calibration["scale"], angle=calibration["angle"])
-images["bg2_scangle"] = ird.transform_img(images["bg2"], 
-    scale=calibration["scale"], angle=calibration["angle"])
-# account for difference in coordinate choice
-(dy, dx) = calibration['tvec']
-dy = round(dy)
-dx = round(dx)
-
-# align N1 and N2 by cropping
+# Use phase correlation to find displacement btwn images
+# and align them through cropping. Using raw images instead of normalized
+# because there are more features to align
+# update 10-19: and using subselection of interest so we align 
+# what we care about
+(dx, dy) = phaseCorrelation.phaseCorrelate(images["align1"], images["align2"],
+                       margin=phasecorr_margin, plotting=phasecorr_plotting)
+# (dx, dy) = phaseCorrelation.phaseCorrelate(images["bg1"], images["bg2"])
+# (dx, dy) = phaseCorrelation.phaseCorrelate(images["N1"], images["N2"])
 (images["N1_aligned"], images["N2_aligned"]) = phaseCorrelation.cropAlign(
-    images["N1"], images["N2_scangle"], dx, dy)
+    images["N1"], images["N2"], dx, dy)
 (images["bg1_aligned"], images["bg2_aligned"]) = phaseCorrelation.cropAlign(
-    images["bg1"], images["bg2_scangle"], dx, dy)
+    images["bg1"], images["bg2"], dx, dy)
+
+#subtract normalized images to eliminate self-emission
+#D = I_S+(x,y)/I_B+(x,y) - I_S-(x,y)/I_B-(x,y) = N1 - N2
+images["D"] = images["N1_aligned"] - images["N2_aligned"]
 
 #%%
 """
@@ -159,11 +115,10 @@ MATH
 def getRotation(probe_ratio, D, beta):
     return 1/2*np.arcsin(-probe_ratio*D/(2*np.tan(beta)))   #beta from full
     # return 1/2*np.arcsin(-probe_ratio*D*np.tan(beta)/2)   #beta from extinct
-       
-#subtract normalized images to eliminate self-emission
-#D = I_S+(x,y)/I_B+(x,y) - I_S-(x,y)/I_B-(x,y) = N1 - N2
-images["D"] = images["N1_aligned"] - images["N2_aligned"]
-    
+
+#for testing, just import the D image created by hand in imageJ
+# D = np.array(Image.open(getfile("Select D")))
+             
 # get rotation angle alpha from D, polarizer angle beta, and  
 # ratio of background to shot probe beam intensities, probe_ratio
 # placeholder for now TODO: find method to obtain (laser energy meter?)
@@ -192,11 +147,10 @@ plt.show()
 """
 IMAGE EXPORT
 """
-save_images = ["N1_aligned","N2_aligned", "D", "alpha_deg", "B"]
-# create output folder if needed
-os.makedirs(folder+"out", exist_ok=True)
+save_images = ["bg1_aligned", "bg2_aligned", "N1_aligned",
+               "N2_aligned", "D", "alpha_deg", "B"]
 for name in save_images:
-    Image.fromarray(images[name]).save(folder+"out/"+dateshot+"_"+name+".tif")
+    Image.fromarray(images[name]).save(folder+dateshot+"_"+name+".tif")
 
 
 
