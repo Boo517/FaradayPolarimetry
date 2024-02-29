5536cee17a47492ff9046d3ccd3eff121cd16a82
unknown <noahdilworth517@gmail.com>
2024-02-29 12:33:19 -0800

    created harriscorner_test.py

---
+++ harriscorner_test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  2 12:28:10 2024
+
+@author: P3 Lab Office
+"""
+#%%
+"""
+IMPORTS
+"""
+import cv2
+import file_ui_utils as utils
+
+#%%
+"""
+MAIN
+"""
+ui = utils.UI()
+ui.getfile("Choose an image for corner detection")
